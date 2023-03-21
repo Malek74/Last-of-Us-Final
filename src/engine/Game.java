a718d81640fa0a3ee40bccb905557a5a93a2af38
Abdelrahman24 <abdelrahmanmohamed237070@gmail.com>
2023-03-21 23:33:57 +0200

    Game class

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,5 +1,7 @@
 package engine;
 
+import java.io.*;
+import java.util.*;
 import java.util.ArrayList;
 import model.characters.Hero;
 import model.characters.Zombie;
@@ -11,9 +13,21 @@ public class Game {
     public static ArrayList<Zombie> zombies;
     public static Cell [][] map;
 
-    public static void loadHeros(String filePath){
+    public static void loadHeros(String filePath)throws Exception {
         String hero;
+        int i = 0;
+        Scanner sc = new Scanner(new File("C:\\GUC\\Spring 2023 (4)\\CSEN 401 (Computer Programming Lab)\\GameProject\\Last-of-Us-Final\\test_heros"));  
+        sc.useDelimiter(",");
+        //sets the delimiter pattern (separation of different cells in the csv file)
+        while(sc.hasNext()){
+        	availableHeros.set(i,sc.next());
+        	//find and adds the next complete token from this scanner
+        	i+=1;
+        }
+        sc.close();
     }
+  //File Reader Citation: https://www.javatpoint.com/how-to-read-csv-file-in-java
+ // File Reader Citation: https://www.geeksforgeeks.org/java-filereader-class-read-method-with-examples/?ref=rp
 
     
 }
