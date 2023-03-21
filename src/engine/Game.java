07d325541c73f3999a27d78ca3f258319104e1c4
Malek74 <65236391+Malek74@users.noreply.github.com>
2023-03-22 00:31:33 +0200

    done

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -4,7 +4,7 @@ import java.io.*;
 import java.util.*;
 import java.util.ArrayList;
 import model.characters.Hero;
-import model.characters.Zombie;
+import model.characters.*;
 import model.world.Cell;
 
 public class Game {
@@ -13,19 +13,50 @@ public class Game {
     public static ArrayList<Zombie> zombies;
     public static Cell [][] map;
 
-    public static void loadHeros(String filePath)throws Exception {
-        String hero;
-        int i = 0;
-        Scanner sc = new Scanner(new File("C:\\GUC\\Spring 2023 (4)\\CSEN 401 (Computer Programming Lab)\\GameProject\\Last-of-Us-Final\\test_heros"));  
-        sc.useDelimiter(",");
-        //sets the delimiter pattern (separation of different cells in the csv file)
-        while(sc.hasNext()){
-        	availableHeros.set(i,sc.next());
-        	//find and adds the next complete token from this scanner
-        	i+=1;
+    public static void main(String[] args){
+        try {
+            loadHeros("src\\engine\\test_heros.csv",availableHeros);
+        } catch (Exception e) {
+            // TODO: handle exception
         }
-        sc.close();
     }
+    public static void loadHeros(String filePath,ArrayList<Hero> availableHeros)throws Exception {
+        int i = 0;
+        
+            Scanner sc = new Scanner(new File(filePath));
+
+            Hero hero;
+            String[] heroData= new String[5];
+            while(sc.hasNext()){
+    
+                heroData=sc.next().split(",");
+                switch (heroData[1]){
+                    case "FIGH":
+                    hero=new Fighter(heroData[0],Integer.parseInt( heroData[2]), Integer.parseInt( heroData[4]),Integer.parseInt( heroData[3]));
+                    break;
+    
+                    case "MED":
+                    hero=new Medic(heroData[0],Integer.parseInt( heroData[2]), Integer.parseInt( heroData[4]),Integer.parseInt( heroData[3]));
+                    break;
+    
+                    case "EXP":
+                    hero=new Explorer(heroData[0],Integer.parseInt( heroData[2]), Integer.parseInt( heroData[4]),Integer.parseInt( heroData[3]));
+                    break;
+    
+                    default:
+                    hero=new Hero(heroData[0],Integer.parseInt( heroData[2]), Integer.parseInt( heroData[4]),Integer.parseInt( heroData[3]));
+    
+                }
+    
+                
+                availableHeros.add(i, hero);
+
+                //find and adds the next complete token from this scanner
+                i++;
+            }
+            sc.close();
+        
+        }
   //File Reader Citation: https://www.javatpoint.com/how-to-read-csv-file-in-java
  // File Reader Citation: https://www.geeksforgeeks.org/java-filereader-class-read-method-with-examples/?ref=rp
 
