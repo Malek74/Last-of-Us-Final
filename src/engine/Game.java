279ec23f603e8c964c872c86c5e2c7002b22fb77
Malek74 <65236391+Malek74@users.noreply.github.com>
2023-04-30 19:28:38 +0300

    hello

--- src/engine/Game.java
+++ src/engine/Game.java
@@ -1,89 +1,117 @@
 package engine;
 
-import java.awt.Point;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.awt.*;
+
+import java.util.Random.*;
 import model.characters.Explorer;
 import model.characters.Fighter;
 import model.characters.Hero;
 import model.characters.Medic;
 import model.characters.Zombie;
-import model.collectibles.Vaccine;
 import model.world.Cell;
-import model.world.CharacterCell;
-import model.world.CollectibleCell;
-import exceptions.*;
 
 public class Game {
-
-	public static Cell[][] map = new Cell[15][15];
-	public static ArrayList<Hero> availableHeroes = new ArrayList<Hero>();
-	public static ArrayList<Hero> heroes = new ArrayList<Hero>();
-	public static ArrayList<Zombie> zombies = new ArrayList<Zombie>();
-
-
-
-	public static void main(String[] args) throws InvalidTargetException , NotEnoughActionsException
-	{
-		Explorer f = new Explorer("malek", 100, 20, 5);
-		f.setLocation(new Point(0,0));
-		map[0][0]=new CharacterCell(f);
-		Zombie z = new Zombie();
-		z.setLocation(new Point(0, 1));
-		map[0][1]=new CharacterCell(z);
-		f.setTarget(z);
-		f.setSpecialAction(true);
-		f.attack();
-
-		
-		System.out.println(f.getActionsAvailable());
 	
-	}
-
-
-	public static void createCharacter(Character c){
-		int x=(int)(Math.random()*15) +1;
-		int y=(int)(Math.random()*15) +1;
-
-		
-
-	}
-
-	public static void endTurn(){
-	}
-
+	public static Cell [][] map ;
+	public static ArrayList <Hero> availableHeroes = new ArrayList<Hero>();
+	public static ArrayList <Hero> heroes =  new ArrayList<Hero>();
+	public static ArrayList <Zombie> zombies =  new ArrayList<Zombie>();
 	
-
-
-	public static void loadHeroes(String filePath) throws IOException {
-
+	
+	
+		
+	public static void loadHeroes(String filePath)  throws IOException {
+		
+		
 		BufferedReader br = new BufferedReader(new FileReader(filePath));
 		String line = br.readLine();
 		while (line != null) {
 			String[] content = line.split(",");
-			Hero hero = null;
+			Hero hero=null;
 			switch (content[1]) {
-				case "FIGH":
-					hero = new Fighter(content[0], Integer.parseInt(content[2]), Integer.parseInt(content[4]),
-							Integer.parseInt(content[3]));
-					break;
-				case "MED":
-					hero = new Medic(content[0], Integer.parseInt(content[2]), Integer.parseInt(content[4]),
-							Integer.parseInt(content[3]));
-					break;
-				case "EXP":
-					hero = new Explorer(content[0], Integer.parseInt(content[2]), Integer.parseInt(content[4]),
-							Integer.parseInt(content[3]));
-					break;
+			case "FIGH":
+				hero = new Fighter(content[0], Integer.parseInt(content[2]), Integer.parseInt(content[4]), Integer.parseInt(content[3]));
+				break;
+			case "MED":  
+				hero = new Medic(content[0], Integer.parseInt(content[2]), Integer.parseInt(content[4]), Integer.parseInt(content[3])) ;
+				break;
+			case "EXP":  
+				hero = new Explorer(content[0], Integer.parseInt(content[2]), Integer.parseInt(content[4]), Integer.parseInt(content[3]));
+				break;
 			}
 			availableHeroes.add(hero);
 			line = br.readLine();
-
+			
+			
 		}
 		br.close();
 
+		
+		
 	}
+
+	private static Point randomPoint()
+	{
+		Random rand = new Random();
+
+		int randomX = rand.nextInt(16);
+		int randomY = rand.nextInt(16);
+		while(map[randomX][randomY] != null)
+		{
+			randomX = rand.nextInt(16);
+			randomY = rand.nextInt(16);
+		}
+		return new Point(randomX,randomY);
+
+	}
+
+
+	public static void startGame (Hero h)
+	{
+		map = new Cell[15][15];
+		map[0][0] = new CharacterCell(h);
+		heroes.add(h);
+		availableHeroes.remove(h);
+		for(int i = 0; i < 5; i++)
+		{
+			Vaccine vaccine = new Vaccine();
+			Point vaccinePoint = randomPoint();
+			map[(int) vaccinePoint.getX()][(int) vaccinePoint.getY()] = new CollectibleCell(vaccine);
+
+			TrapCell trap = new TrapCell();
+			Point trapPoint =  randomPoint();
+			map[(int) trapPoint.getX()][(int) trapPoint.getY()] = trap;
+
+			Supply supply = new Supply();
+			Point supplyPoint = randomPoint();
+			map[(int) supplyPoint.getX()][(int) supplyPoint.getY()] = new CollectibleCell(supply);
+
+			for(int j = 0; j<2; j++){
+				Zombie zombie = new Zombie();
+				Point zombieLocation = randomPoint();
+				map[(int) zombieLocation.getX()][(int) zombieLocation.getY()] = CharacterCell(zombie);
+			}
+		}
+	}
+
+	public static boolean checkWin()
+	{
+		return heroes.size() == 5;
+	}
+
+	public static boolean checkGameOver()
+	{
+		return heroes.size() == 0;
+	}
+
+	public static void endTurn()
+	{
+		
+	}
+
 
 }
