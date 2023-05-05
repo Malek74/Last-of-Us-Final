b906db46f12dec4f97cf737beb49b8244f34d86d
Abdelrahman24 <abdelrahmanmohamed237070@gmail.com>
2023-05-05 14:06:57 +0300

    interface + comment(question)

--- src/model/collectibles/Vaccine.java
+++ src/model/collectibles/Vaccine.java
@@ -15,7 +15,7 @@ public class Vaccine implements Collectible {
 	}
 	public void use(Hero h){
 		h.getVaccineInventory().remove(0);
-
+		// should we add here no available resources exception or the item is already in our inventory?
 	}
 
 
