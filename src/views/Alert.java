3ebc5c483e266b91b99126599703ab1525b311ca
Malek74 <65236391+Malek74@users.noreply.github.com>
2023-05-23 14:19:19 +0300

    doneee

--- src/views/Alert.java
+++ src/views/Alert.java
@@ -18,104 +18,95 @@ import javafx.util.Duration;
 
 public class Alert extends Label {
 	private PauseTransition pause = new PauseTransition(Duration.seconds(3));
-    private Node root;
+	private Node root;
 
-	
-	public Alert(){
+	// sddd
+	public Alert() {
 		super();
 		this.setVisible(false);
-		this.setFont(Font.font("Impact",25));
+		this.setFont(Font.font("Impact", 25));
 		this.setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
 		this.setTextFill(Color.WHITE);
 		this.setTranslateZ(5);
 		this.setAlignment(Pos.CENTER);
-        this.setPadding(new Insets(10));
-        this.setStyle("-fx-border-color: white;");
-        
-        
+		this.setPadding(new Insets(10));
+		this.setStyle("-fx-border-color: white;");
+
 	}
-	
-	public Alert(StackPane grid,String text){
+
+	public Alert(StackPane grid, String text) {
 		super();
 		this.setVisible(false);
-		this.setFont(Font.font("Verdana",25));
+		this.setFont(Font.font("Verdana", 25));
 		this.setBackground(new Background(new BackgroundFill(Color.GRAY, null, null)));
 		this.setTextFill(Color.WHITE);
 		this.setTranslateZ(1);
 		this.setAlignment(Pos.CENTER);
-        this.setPadding(new Insets(10));
-        this.setStyle("-fx-border-color: white;");
-        grid.getChildren().add(this);
-        this.setText(text);
-        
+		this.setPadding(new Insets(10));
+		this.setStyle("-fx-border-color: white;");
+		grid.getChildren().add(this);
+		this.setText(text);
+
 	}
-	
-	public void popALert(StackPane root){
+
+	public void popALert(StackPane root) {
 		this.setVisible(true);
 		System.out.println("JELLO");
 		root.setMouseTransparent(true);
-		
+
 		final EventHandler<KeyEvent> disableKeysHandler = event -> {
-            if (this.isVisible()) {
-                event.consume();
-            }
-        };
+			if (this.isVisible()) {
+				event.consume();
+			}
+		};
 		root.addEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
-        root.addEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
-        StartGame.grid.addEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
-        StartGame.grid.addEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
-        StartGame.gameScreen.addEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
-        StartGame.gameScreen.addEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
-        
-		
+		root.addEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
+		StartGame.grid.addEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
+		StartGame.grid.addEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
+		StartGame.gameScreen.addEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
+		StartGame.gameScreen.addEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
+
 		pause.setOnFinished(event -> {
 			this.setVisible(false);
 			root.setMouseTransparent(false);
-	        root.removeEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
-	        root.removeEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
-	        
-	        
-	        StartGame.grid.removeEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
-	        StartGame.grid.removeEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
-	        
-	        StartGame.gameScreen.removeEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
-	        StartGame.gameScreen.removeEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
-	        
-			});
-        
+			root.removeEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
+			root.removeEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
+
+			StartGame.grid.removeEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
+			StartGame.grid.removeEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
+
+			StartGame.gameScreen.removeEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
+			StartGame.gameScreen.removeEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
+
+		});
+
 		pause.playFromStart();
-		
+
 	}
-	
-	public void popALert(){
+
+	public void popALert() {
 		this.setVisible(true);
-		this.root=this.getParent();
+		this.root = this.getParent();
 		System.out.println("JELLO");
 		root.setMouseTransparent(true);
-		
+
 		final EventHandler<KeyEvent> disableKeysHandler = event -> {
-            if (this.isVisible()) {
-                event.consume();
-            }
-        };
+			if (this.isVisible()) {
+				event.consume();
+			}
+		};
 		root.addEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
-        root.addEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
-        
-        
-		
+		root.addEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
+
 		pause.setOnFinished(event -> {
 			this.setVisible(false);
 			root.setMouseTransparent(false);
-	        root.removeEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
-	        root.removeEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
-			});
-        
+			root.removeEventFilter(KeyEvent.KEY_PRESSED, disableKeysHandler);
+			root.removeEventFilter(KeyEvent.KEY_RELEASED, disableKeysHandler);
+		});
+
 		pause.playFromStart();
-		
+
 	}
-	
-	
-	
-	
 
 }
